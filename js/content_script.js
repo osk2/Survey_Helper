476b4b1ff82576ca6358d64f03d7930d841aa6c1
osk2 <sc60714@gmail.com>
2015-03-03 15:42:27 +0800

    replace confirm box with sweet-alert.js

--- js/content_script.js
+++ js/content_script.js
@@ -1,6 +1,13 @@
 ﻿switch (window.location.hostname) {
 	case 'identity.stust.edu.tw':
 		//login first
+		swal({
+			title: '噢！',
+			text: '( ﾟ∀ﾟ)o彡ﾟ先登入才能幫你寫問卷哦',
+			confirmButtonText: '好啦',
+			confirmButtonColor: '#446CB3',
+			type: 'info'
+		});
 		break;
 
 	case 'eportal.stust.edu.tw':
@@ -22,9 +29,19 @@
 
 					for (i = 0; i <= count; ++i) {
 						if (i === count) {
-							if (confirm('全部搞定啦！\n\n要帶你到成績查詢頁面嗎？A__A')) {
+							swal({
+								title: '全部搞定啦！',
+								text: '要帶你到成績查詢頁面嗎？ლ(・´ｪ`・ლ)',
+								showCancelButton: true,
+								confirmButtonText: '走吧',
+								confirmButtonColor: '#446CB3',
+								cancelButtonText: '不了…',
+								cancelButtonColor: '#2C3E50',
+								type: 'success'
+							},
+							function() {
 								document.location.href = 'http://120.117.2.132/CourSel/Pages/PresentScore.aspx';
-							}
+							});
 							break;
 						}
 						target = $('#ctl00_ContentPlaceHolder1_dlPollStatus > tbody > tr:eq('+ i +')').find(':submit');
