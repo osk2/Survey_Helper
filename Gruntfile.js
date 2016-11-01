774d392ca393c3410e2729b40684f08ebd424d32
osk2 <sc60714@gmail.com>
2016-11-01 19:35:40 +0800

    add grunt task to upload to chrome store automatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        key: grunt.file.readJSON('key.json'),
         uglify: {
             options: {
                 banner: '/*\n  <%= pkg.description %> V<%= pkg.version %>\n  Build Time <%= grunt.template.today("yyyy-mm-dd HH:MM") %>\n  https://github.com/osk2/Survey_Helper\n*/\n'
@@ -13,12 +14,34 @@ module.exports = function(grunt) {
         },
         jshint: {
             all: ['js/content_script.js']
+        },
+        zip: {
+            '<%= pkg.name %>.zip': ['css/**', 'icons/**', 'js/background.js', 'js/content_script.min.js', 'js/jquery-2.0.3.min.js', 'js/jquery-2.0.3.min.map', 'js/sweet-alert.min.js', 'manifest.json', 'background.html', 'LICENSE']
+        },
+        webstore_upload: {
+            'accounts': {
+                'default': {
+                    publish: false,
+                    client_id: '<%= key.client_id %>',
+                    client_secret: '<%= key.client_secret %>'
+                }
+            },
+            'extensions': {
+                'survey_helper': {
+                    appID: '<%= key.appID %>',
+                    zip: '<%= pkg.name %>.zip'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-webstore-upload');
 
     grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('pack', ['zip']);
+    grunt.registerTask('deploy', ['zip', 'webstore_upload']);
 
 };
